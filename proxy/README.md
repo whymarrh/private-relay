47958a2dbe24df0ddaa3e9d8654bf5b4a9e8effb
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2020-07-06 14:11:25 -0230

    Multiple backends for each HAProxy instance

--- proxy/README.md
+++ proxy/README.md
@@ -2,25 +2,21 @@
 
 ## Quick start
 
-Build the images:
+Build the HAProxy config and image:
 
 ```bash
-# A GitHub API relay
-PRIVATE_RELAY_ORIGIN_SERVER_ADDRESS='api.github.com:443' \
-docker build --build-arg PRIVATE_RELAY_ORIGIN_SERVER_ADDRESS --tag private-relay:github .
-
-# A httpbin.org relay
-PRIVATE_RELAY_ORIGIN_SERVER_ADDRESS='httpbin.org:443' \
-docker build --build-arg PRIVATE_RELAY_ORIGIN_SERVER_ADDRESS --tag private-relay:httpbin .
+terraform init
+terraform apply -auto-approve # To build the HAProxy config
+docker build --tag private-relay .
 ```
 
-To run the images:
+To run HAProxy:
 
 ```bash
-terraform apply
+docker run --rm --detach --publish 8080:443 private-relay
 ```
 
-To test the images:
+To test the config:
 
 ```
 pushd test/
@@ -29,14 +25,8 @@ yarn
 GITHUB_API_ENDPOINT='https://api.github.com' \
 GITHUB_API_ENDPOINT_RELAY="https://127.0.0.1:8080" \
 HTTPBIN_ENDPOINT="https://httpbin.org" \
-HTTPBIN_ENDPOINT_RELAY="https://127.0.0.1:9090" \
+HTTPBIN_ENDPOINT_RELAY="https://127.0.0.1:8080" \
 yarn test
 
 popd
-```
-
-To remove the running containers:
-
-```bash
-terraform destroy
 ```
--- proxy/main.tf
+++ proxy/main.tf
@@ -1,35 +1,23 @@
-provider "docker" {
-  version = "~> 2.7"
+provider "local" {
+  version = "~> 1.4"
 }
 
-resource "docker_image" "private_relay_github" {
-  name         = "private-relay:github"
-  keep_locally = true
-}
+resource "local_file" "haproxy_config" {
+  filename        = "${path.module}/haproxy.cfg"
+  file_permission = "0644"
 
-resource "docker_image" "private_relay_httpbin" {
-  name         = "private-relay:httpbin"
-  keep_locally = true
-}
-
-resource "docker_container" "private_relay_github" {
-  image    = docker_image.private_relay_github.latest
-  name     = "private-relay-github"
-  must_run = true
-  restart  = "on-failure"
-  ports {
-    internal = 443
-    external = 8080
-  }
-}
-
-resource "docker_container" "private_relay_httpbin" {
-  image    = docker_image.private_relay_httpbin.latest
-  name     = "private-relay-httpbin"
-  must_run = true
-  restart  = "on-failure"
-  ports {
-    internal = 443
-    external = 9090
-  }
+  content = templatefile("${path.module}/haproxy.cfg.tmpl", {
+    backends = [
+      {
+        name = "github"
+        host = "api.github.com"
+        port = "443"
+      },
+      {
+        name = "httpbin"
+        host = "httpbin.org"
+        port = "443"
+      },
+    ]
+  })
 }
