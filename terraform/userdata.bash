1e120b92e42a6dca67cbd17be026c320aed1b89b
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2020-07-10 10:43:11 -0230

    Update run command for HAProxy
    After load testing multiple instance sizes the following flags are now passed
    to the HAProxy container:
    
    ```
    --sysctl net.ipv4.ip_local_port_range="1024 65535"
    --sysctl net.ipv4.tcp_tw_reuse=1
    ```
    
    Test results: https://gist.github.com/2ff036bfbcc6890915a56e9deceda43a

--- terraform/userdata.bash
+++ terraform/userdata.bash
@@ -26,7 +26,12 @@ apt install --yes \
 # This comes from Terraform
 # shellcheck disable=SC2016
 IMAGE='${docker_image_name}'
-docker run --detach --name private-relay --restart 'on-failure' --publish 443:443 "$IMAGE"
+docker run --detach --name private-relay \
+    --restart 'unless-stopped' \
+    --publish 443:443 \
+    --sysctl net.ipv4.ip_local_port_range="1024 65535" \
+    --sysctl net.ipv4.tcp_tw_reuse=1 \
+    "$IMAGE"
 docker run --detach --name watchtower \
     --volume /var/run/docker.sock:/var/run/docker.sock \
     containrrr/watchtower \
