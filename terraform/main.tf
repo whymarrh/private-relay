f7b2528ad8e7d630d542accb01a154ca4356cd41
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2020-10-25 19:48:59 -0230

    Update cloudflare/cloudflare provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 2.7"
+      version = "~> 2.12"
     }
     digitalocean = {
       source  = "digitalocean/digitalocean"
