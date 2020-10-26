e88cb16a7a657ddb1ad625137142c90e24bdd5fc
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2020-10-25 21:51:45 -0230

    Shrink root example to a single droplet

--- terraform/main.tf
+++ terraform/main.tf
@@ -59,60 +59,6 @@ variable "private_relay_docker_image_name" {
   type        = string
 }
 
-variable "region_map_keys" {
-  description = "The set of Cloudflare origin pool regions"
-
-  # The ordering of the pools in the load balancer determines the order in
-  # which pools in the load balancer will fail over. When the number of
-  # healthy origins within a pool goes below the configured threshold,
-  # Cloudflare will send traffic to the next available pool - e.g. traffic
-  # will always land on Pool #1 until it is marked unhealthy.
-  default = [
-    "eu",
-    "na",
-    "ap",
-  ]
-}
-
-variable "region_map_values" {
-  description = "The set of supported regions"
-  default = [
-    {
-      # EU region
-      do_regions = ["ams3", "fra1"]
-      check_regions = [
-        "WNAM", "ENAM", # North America
-        "SSAM",         # South America
-        "OC",           # Oceania
-        "WEU", "EEU",   # Europe
-        "SEAS", "NEAS", # Asia
-      ]
-    },
-    {
-      # North American region
-      do_regions = ["tor1", "sfo3"]
-      check_regions = [
-        "WNAM", "ENAM", # North America
-        "SSAM",         # South America
-        "OC",           # Oceania
-        "WEU", "EEU",   # Europe
-        "SEAS", "NEAS", # Asia
-      ]
-    },
-    {
-      # Asia-Pacific region
-      do_regions = ["sgp1"]
-      check_regions = [
-        "WNAM", "ENAM", # North America
-        "SSAM",         # South America
-        "OC",           # Oceania
-        "WEU", "EEU",   # Europe
-        "SEAS", "NEAS", # Asia
-      ]
-    },
-  ]
-}
-
 module "private_relay" {
   source                          = "./modules/cloudflare-digitalocean"
   name                            = "private-relay"
@@ -120,4 +66,17 @@ module "private_relay" {
   cf_zone_id                      = var.cf_zone_id
   cf_lb_name                      = var.cf_lb_name
   private_relay_docker_image_name = var.private_relay_docker_image_name
+
+  # A single Asia-Pacific region with a single droplet
+  region_map_keys = ["ap"]
+  region_map_values = [{
+    do_regions = ["sgp1"]
+    check_regions = [
+      "WNAM", "ENAM", # North America
+      "SSAM",         # South America
+      "OC",           # Oceania
+      "WEU", "EEU",   # Europe
+      "SEAS", "NEAS", # Asia
+    ]
+  }]
 }
